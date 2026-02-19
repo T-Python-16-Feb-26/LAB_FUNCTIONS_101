cf38d83039100565e41316cb8156dd813fd50c2e
Araa Al-Marhabi <144210813+almarhabiaraa@users.noreply.github.com>
2026-02-19 12:11:58 +0300

    Add documentation and print it after calling the function

--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
-
-
 def pattern(rows):
+    """
+    This function prints a pattern of numbers where each row starts 
+    from the current row number down to 1, separated by spaces.
+    The number of rows is determined by the 'rows' parameter.
 
+    """
     for i in range(rows, 0, -1):
         for j in range(i, 0, -1):
             print(j, end=" ")
         print()
 
 pattern(5)
+
+print(pattern.__doc__)
