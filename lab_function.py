8bf60b3ce368d7f4228840de4e1224987a1d47ba
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-02-19 12:03:28 +0300

    lab done + bonus

--- lab_function.py
+++ lab_function.py
@@ -1,5 +1,5 @@
 def make_tower(num:int):
-    """takes 1 parameter of type int, prints out the result formatted as a tower"""
+    """takes 1 parameter of type int, prints out the result formatted as a tower """
     for i in range(num, 0, -1):
         temp = ""
         for j in range(i, 0, -1):
